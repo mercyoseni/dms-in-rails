47fdef483ffb8e98adb08d22e35894a18927f75c
andela-moseni <mercy.oseni@andela.com>
2018-06-04 13:38:45 +0100

    Update password validation and partially fix validation issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates_presence_of :firstname, :lastname, :email, :role
   validates_inclusion_of :role, in: %w(admin regular author contributor)
   validates_uniqueness_of :email
-  validates_length_of :email, :password, in: 6..50
+  validates_length_of :email, in: 6..50
+  validates_length_of :password, in: 6..20, if: :password
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end

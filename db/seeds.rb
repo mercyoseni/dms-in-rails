d223d21c14da21d0be415d77d5f8a1f32579066b
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-03-17 16:08:58 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,23 @@
-5.times do
+p 'seeding data'
+
+10.times do
   User.create({
     firstname: Faker::Name.first_name,
     lastname: Faker::Name.last_name,
     email: Faker::Internet.email,
-    role: %w(admin regular author contributor).sample
+    password: 'password',
   })
 end
 
-5.times do
+p 'created ten(10) users'
+
+20.times do
   Document.create({
     title: Faker::Book.title,
     body: Faker::Lorem.paragraph,
     access: %w(public private role_based).sample,
-    user_id: %w(1 2 3).sample.to_i
+    user_id: User.ids.sample,
   })
 end
+
+p 'created twenty(20) documents'

6ce701bf8f002a56b28c11ebe1e80f5d31dbdce7
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-08-13 19:22:36 +0100

    Update document CRUD actions - Serialize response to be json:api compliant

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,59 +1,59 @@
 class Api::V1::DocumentsController < ApplicationController
   before_action :find_document, only: [:show, :update, :destroy]
   before_action :is_current_user_document, only: [:update, :destroy]
-  before_action :check_params, only: [:update]
 
   def index
-    @documents = current_user_and_public_documents + current_user_role_based_documents
+    documents =
+      current_user_and_public_documents + current_user_role_based_documents
 
-    response = { message: Message.loaded_documents, data: @documents.uniq }
+    response = resource_serializer(resource, documents)
+
     json_response(response)
   end
 
   def show
-    response = { message: Message.loaded_document, data: @document }
+    response = resource_serializer(resource, @document)
     json_response(response)
   end
 
   def create
     # create documents belonging to a user
-    @document = current_user.documents.create!(document_params)
-    response = { message: Message.created_document, data: @document }
+    document = current_user.documents.create!(document_params)
+    response = resource_serializer(resource, document)
+
     json_response(response, :created)
   end
 
   def update
     @document.update_attributes!(document_params)
-    response = { message: Message.updated_document, data: @document }
+    response = resource_serializer(resource, @document)
+
     json_response(response)
   end
 
   def destroy
     @document.destroy
-    response = { message: Message.deleted_document }
-    json_response(response)
   end
 
   private
 
   def is_current_user_document
-    raise ExceptionHandler::Forbidden unless @document.user_id == current_user.id
+    if @document.user_id != current_user.id
+      raise ExceptionHandler::Forbidden
+    end
   end
 
   def find_document
     @document = current_user_and_public_documents.find(params[:id]) ||
-                current_user_role_based_documents.find(params[:id])
+      current_user_role_based_documents.find(params[:id])
   end
 
   def document_params
-    params.permit(:title, :body, :access)
+    params.require(:data).require(:attributes).permit(:title, :body, :access)
   end
 
-  def check_params
-    if params[:document][:id] || params[:document][:user_id]
-      response = { message: Message.not_allowed }
-      json_response(response, 403)
-    end
+  def resource
+    Api::V1::DocumentResource
   end
 
   def current_user_and_public_documents

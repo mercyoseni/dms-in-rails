3d33f767079e32fcb332b43ca5396ee70ae8bae8
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-08-13 19:17:31 +0100

    Validate document body length

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,5 +3,5 @@ class Document < ApplicationRecord
 
   validates_presence_of :title, :body, :access, :user
   validates_inclusion_of :access, in: %w(public private role_based)
-  validates_length_of :title, in: 5..100
+  validates_length_of :title, :body, in: 5..100
 end

498938b237e13924e9bbd8bab5a0f933c6e5bde2
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-08-13 19:19:12 +0100

    Update doc resource attributes

--- app/resources/api/v1/document_resource.rb
+++ app/resources/api/v1/document_resource.rb
@@ -1,7 +1,7 @@
 class Api::V1::DocumentResource < Api::V1::ApplicationResource
   model_name 'Document'
 
-  attributes :title, :body, :access
+  attributes :title, :body, :access, :created_at, :updated_at
   has_one :user
   filters :title, :body, :access
 end

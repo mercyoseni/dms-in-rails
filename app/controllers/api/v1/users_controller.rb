e7d2973c0a60fb8d14852b21285f65368d02743c
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-08-13 13:51:46 +0100

    Update index, show and update actions - Serialize response to be json:api compliant

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,21 +4,35 @@ class Api::V1::UsersController < ApplicationController
   before_action :check_password_params, only: :update
 
   def index
-    @users = User.select('id, firstname, lastname, email').order('created_at ASC')
-    response = { message: Message.loaded_users, data: @users }
+    users = User.refined_users
+    serialized_users =
+      users.map { |user| Api::V1::UserResource.new(user, nil) }
+    response = JSONAPI::ResourceSerializer.new(
+      Api::V1::UserResource,
+      fields: {
+        users: [:firstname, :lastname, :email]
+      }
+    ).serialize_to_hash(serialized_users)
+
     json_response(response)
   end
 
   def show
     if current_user.id.to_s == params[:id]
-      @user = User.find(params[:id])
-      response = { message: Message.loaded_user, data: @user }
-      json_response(response)
+      user = User.find(params[:id])
+      response = JSONAPI::ResourceSerializer.new(Api::V1::UserResource)
+        .serialize_to_hash(Api::V1::UserResource.new(user, nil))
     else
-      @user = User.select('id, firstname, lastname, email').find(params[:id])
-      response = { message: Message.loaded_user, data: @user }
-      json_response(response)
+      user = User.refined_users.find(params[:id])
+      response = JSONAPI::ResourceSerializer.new(
+        Api::V1::UserResource,
+        fields: {
+          users: [:firstname, :lastname, :email]
+        }
+      ).serialize_to_hash(Api::V1::UserResource.new(user, nil))
     end
+
+    json_response(response)
   end
 
   def create
@@ -27,18 +41,15 @@ class Api::V1::UsersController < ApplicationController
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
+
     json_response(response, :created)
   end
 
   def update
     @user.update_attributes!(user_params)
-    response = { message: Message.updated_user, data: @user }
-    json_response(response)
-  end
+    response = JSONAPI::ResourceSerializer.new(Api::V1::UserResource)
+      .serialize_to_hash(Api::V1::UserResource.new(@user, nil))
 
-  def destroy
-    @user.destroy
-    response = { message: Message.deleted_user }
     json_response(response)
   end
 
